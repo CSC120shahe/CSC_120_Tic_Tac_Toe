d304e421a03bc34f1173fb508314f8e1c7439a78
Sha He <shahe@Shas-MacBook-Air.local>
2022-07-20 10:33:23 -0400

    Board printing functionality added

---
+++ board.py
@@ -0,0 +1,15 @@
+def print_board():
+  board = []
+  for _ in range(3):
+    board.append(['-', '-', '-'])
+  print("Printing board")
+  for elem in board:
+    print(elem)
+
+print_board()
+  
+  
+  
+  
+  
+  
