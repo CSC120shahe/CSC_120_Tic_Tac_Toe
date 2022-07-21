460c7d51b5cd8b175a24c78e4c0f2035aeaa265c
Sha He <shahe@Shas-MacBook-Air.local>
2022-07-21 00:15:41 -0400

    refine code for board print and create

--- board.py
+++ board.py
@@ -1,12 +1,18 @@
-def print_board():
-  board = []
-  for _ in range(3):
-    board.append(['-', '-', '-'])
-  print("Printing board")
+def print_board(board):
   for elem in board:
     print(elem)
 
-print_board()
+# create a tic-tac-toe board
+board = []
+for _ in range(3):
+  board.append(['-', '-', '-'])
+  
+print_board(board)
+
+
+
+
+
   
   
   
